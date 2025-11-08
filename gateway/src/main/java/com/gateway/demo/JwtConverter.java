289e018b8cee343ec10666814abec074e4b6c147
PepinilloJR <pepinillosverdes86@gmail.com>
2025-11-08 14:54:36 -0300

    redirecciones

--- gateway/src/main/java/com/gateway/demo/JwtConverter.java
+++ gateway/src/main/java/com/gateway/demo/JwtConverter.java
@@ -5,10 +5,12 @@ package com.gateway.demo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
@@ -20,11 +22,12 @@ public class JwtConverter implements Converter<Jwt, AbstractAuthenticationToken>
 
         Map<String, List<String>> realmAccess = jwt.getClaim("realm_access");
 
-        List roles = realmAccess.get("roles");
+        //simplegranted Authority es la implementacion de grantedAuthority
+        ArrayList<GrantedAuthority> roles = realmAccess.get("roles").stream().map(r -> "ROLE_" + r.toUpperCase()).map(g -> new SimpleGrantedAuthority(g)).collect(Collectors.toCollection(ArrayList::new));
 
         System.out.println(realmAccess.toString());
         //ArrayList<GrantedAuthority> autoridades = roles.stream().map()
-        return new JwtAuthenticationToken(jwt, new ArrayList<GrantedAuthority>());
+        return new JwtAuthenticationToken(jwt, roles);
     }
 
 }
--- gateway/src/main/java/com/gateway/demo/ResourceServerConfig.java
+++ gateway/src/main/java/com/gateway/demo/ResourceServerConfig.java
@@ -1,22 +1,24 @@
 package com.gateway.demo;
 
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 @Configuration
-@EnableWebSecurity
-@EnableMethodSecurity
 public class ResourceServerConfig {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         
         http.authorizeHttpRequests(auth -> 
         auth.requestMatchers("/protected/**")
-
-        .authenticated())
+        .hasRole("ADMIN")
+        .anyRequest()
+        .authenticated()
+        )
         .oauth2ResourceServer(o -> o.jwt().jwtAuthenticationConverter(new JwtConverter()));
         return http.build();
 
