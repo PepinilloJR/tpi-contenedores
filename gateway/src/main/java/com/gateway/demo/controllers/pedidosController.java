ed0985ad59202951898b4d3d94f10f9978877d3b
PepinilloJR <pepinillosverdes86@gmail.com>
2025-11-13 21:38:11 -0300

    httpclients

--- gateway/src/main/java/com/gateway/demo/controllers/pedidosController.java
+++ gateway/src/main/java/com/gateway/demo/controllers/pedidosController.java
@@ -1,5 +1,12 @@
 package com.gateway.demo.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestClient;
+
 public class pedidosController {
-    
+    @Autowired
+    RestClient pedidosClient;
+
+    @Autowired
+    RestClient camionesClient;
 }
