717bd68b06cda475da59ce51fc9e5565e910814c
PepinilloJR <pepinillosverdes86@gmail.com>
2025-11-17 12:24:07 -0300

    automatizacion para subir las imagenes a dockerhub

--- root/autobuild.sh
+++ root/autobuild.sh
@@ -7,6 +7,17 @@ SERVICES=(
   "depositos-service"
 )
 
+echo "=== Checking Docker login using config.json ==="
+if grep -q "index.docker.io" ~/.docker/config.json 2>/dev/null; then
+    echo "Estás logueado en Docker Hub"
+    LOGGED_IN=true
+else
+    echo "No estás logueado en Docker Hub"
+    echo "No se subiran las imagenes"
+    LOGGED_IN=false
+fi
+
+
 for service in "${SERVICES[@]}"; do
     echo "=============================="
     echo "BUILDING: $service"
@@ -17,6 +28,14 @@ for service in "${SERVICES[@]}"; do
     mvn clean package -DskipTests
 
     docker build -t "pepinillojr/tpi-backend:$service" .
+
+    if [ "$LOGGED_IN" = true ]; then
+        echo "Haciendo push de pepinillojr/tpi-backend:$service"
+        docker push "pepinillojr/tpi-backend:$service"
+    else
+        echo "Saltando push"
+    fi
+
 
     cd ..
 done
