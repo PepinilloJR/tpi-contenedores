ad5db8a9ffc5ebee65c703f3a6ab43109f515add
aparicio96070 <96070@sistemas.frc.utn.edu.ar>
2025-11-17 20:03:14 -0300

    fix

--- root/commonlib/src/main/java/com/commonlib/dto/ContenedorDto.java
+++ root/commonlib/src/main/java/com/commonlib/dto/ContenedorDto.java
@@ -1,10 +1,21 @@
 package com.commonlib.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+
 public record ContenedorDto(
         Long id,
+        @NotNull(message = "El peso es obligatorio")
+        @Positive(message = "El peso debe ser mayor a 0")
         Double peso,
+        @NotNull(message = "El volumen es obligatorio")
+        @Positive(message = "El volumen debe ser mayor a 0")
         Double volumen,
+        // Se puede borrar
         String estado,
+        @NotNull(message = "El costo de volumen es obligatorio")
+        @Positive(message = "El costo de volumen debe ser mayor a 0")
         Double costoVolumen
         ) {
 }
--- root/commonlib/src/main/java/com/commonlib/entidades/Contenedor.java
+++ root/commonlib/src/main/java/com/commonlib/entidades/Contenedor.java
@@ -1,6 +1,6 @@
 package com.commonlib.entidades;
 
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,6 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,13 +20,18 @@ import lombok.ToString;
 @Builder
 @ToString
 @Entity
+
 public class Contenedor {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private Double peso;
+    @Column(nullable = false)
     private Double volumen;
+
     private String estado;
+    @Column(nullable = false)
     private Double costoVolumen;
 
 }
